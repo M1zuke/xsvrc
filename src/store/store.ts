4529a1b7257f3215881e37bcbed340331f2cb74d
Tim Hülsmeier <tim.huelsmeier@comline-se.de>
2020-01-27 17:29:39 +0000

    Sorted package.json & added redux logger
    - Added Redux logger or better debuging.
    - Changed dependencies to dev-dependecies which should be there in the
    first place.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './rootReducer';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import { createLogger } from 'redux-logger';
 
 const persistConfig = {
   key: 'store',
@@ -11,6 +12,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer);
-export const persistor = persistStore(store as any);
+export const store = createStore(persistedReducer, applyMiddleware(
+    createLogger(),
+));
+export const persistor = persistStore(store);
 
